da595898d82f427283d6eec47b38aa52dc8acc3d
thieblin <thieblin@MacBook-Pro-de-thieblin.local>
2015-11-20 09:00:22 -0800

    prepare for spring conversions

--- src/main/scala/play/api/inject/spring/SpringInjectorBuilder.scala
+++ src/main/scala/play/api/inject/spring/SpringInjectorBuilder.scala
@@ -29,7 +29,7 @@ object SpringableModule extends SpringableModuleConversions {
    */
   def springable(module: Any): SpringableModule = module match {
     case playModule: Module => fromPlayModule(playModule)
-    case annotatedClass: Class[_] => fromSpringModule(annotatedClass)
+    case annotatedClass: Class[_] => fromSpringClass(annotatedClass)
     case unknown => throw new PlayException(
       "Unknown module type",
       s"Module [$unknown] is not a Play module or a Guice module"
@@ -41,5 +41,73 @@ object SpringableModule extends SpringableModuleConversions {
    */
   def springed(env: Environment, conf: Configuration)(builders: Seq[SpringableModule]): Seq[Class[_]] =
     builders flatMap { module => module.springed(env, conf) }
+
+}
+
+/**
+ * Implicit conversions to SpringableModules.
+ */
+trait SpringableModuleConversions {
+
+  import scala.language.implicitConversions
+
+  implicit def fromSpringClass(annotatedClass: Class[_]): SpringableModule = fromSpringClasses(Seq(annotatedClass))
+
+  implicit def fromSpringClasses(annotatedClasses: Seq[Class[_]]): SpringableModule = new SpringableModule {
+    def springed(env: Environment, conf: Configuration): Seq[Class[_]] = annotatedClasses
+    def disable(classes: Seq[Class[_]]): SpringableModule = fromSpringClasses(filterOut(classes, annotatedClasses))
+    override def toString = s"SpringableModule(${annotatedClasses.mkString(", ")})"
+  }
+
+  implicit def fromPlayModule(playModule: Module): SpringableModule = fromPlayModules(Seq(playModule))
+
+  implicit def fromPlayModules(playModules: Seq[Module]): SpringableModule = new SpringableModule {
+    def springed(env: Environment, conf: Configuration): Seq[Class[_]] = playModules.map(spring(env, conf))
+    def disable(classes: Seq[Class[_]]): SpringableModule = fromPlayModules(filterOut(classes, playModules))
+    override def toString = s"SpringableModule(${playModules.mkString(", ")})"
+  }
+
+  implicit def fromPlayBinding(binding: Binding[_]): SpringableModule = fromPlayBindings(Seq(binding))
+
+  implicit def fromPlayBindings(bindings: Seq[Binding[_]]): SpringableModule = new SpringableModule {
+    def springed(env: Environment, conf: Configuration): Seq[Class[_]] = Seq(spring(bindings))
+    def disable(classes: Seq[Class[_]]): SpringableModule = this // no filtering
+    override def toString = s"SpringableModule(${bindings.mkString(", ")})"
+  }
+
+  private def filterOut[A](classes: Seq[Class[_]], instances: Seq[A]): Seq[A] =
+    instances.filterNot(o => classes.exists(_.isAssignableFrom(o.getClass)))
+
+  /**
+   * Convert the given Play module to a Guice module.
+   */
+  def spring(env: Environment, conf: Configuration)(module: Module): Class[_] =
+    spring(module.bindings(env, conf))
+
+  /**
+   * Convert the given Play bindings to a Guice module.
+   */
+  def spring(bindings: Seq[Binding[_]]): Class[_] = {
+    new com.google.inject.AbstractModule {
+      def configure(): Unit = {
+        for (b <- bindings) {
+          val binding = b.asInstanceOf[PlayBinding[Any]]
+          val builder = binder().withSource(binding).bind(GuiceKey(binding.key))
+          binding.target.foreach {
+            case ProviderTarget(provider) => builder.toProvider(GuiceProviders.guicify(provider))
+            case ProviderConstructionTarget(provider) => builder.toProvider(provider)
+            case ConstructionTarget(implementation) => builder.to(implementation)
+            case BindingKeyTarget(key) => builder.to(GuiceKey(key))
+          }
+          (binding.scope, binding.eager) match {
+            case (Some(scope), false) => builder.in(scope)
+            case (None, true) => builder.asEagerSingleton()
+            case (Some(scope), true) => throw new GuiceLoadException("A binding must either declare a scope or be eager: " + binding)
+            case _ => // do nothing
+          }
+        }
+      }
+    }
+  }
 
 }
